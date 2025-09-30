9738458149e92f0c847ef60f412a86358e200c62
Abh1i <167069151+Abh1i@users.noreply.github.com>
2025-09-30 16:00:09 +0530

    changes new

--- src/app/qr-code-list/page.tsx
+++ src/app/qr-code-list/page.tsx
@@ -2,36 +2,38 @@
 import { useEffect, useState } from 'react';
 
 export default function QrCodeListPage() {
-  const [customer, setCustomer] = useState<any>(null);
+    const [customer, setCustomer] = useState<any>(null);
 
-  useEffect(() => {
-    // Ask parent for data immediately
-    window.parent.postMessage({ type: 'REQUEST_CUSTOMER_INFO' }, '*');
+    useEffect(() => {
+        const handleMessage = (event: MessageEvent) => {
+            if (event.data?.type === 'CUSTOMER_INFO') {
+                setCustomer(event.data.payload); // <-- shows inside iframe
+            }
+        };
+         const timeout = setTimeout(() => {
+            console.log('');
+    }, 2000)
+        window.addEventListener('message', handleMessage);
+        return () => window.removeEventListener('message', handleMessage);
+    }, []);
 
-    const handleMessage = (event: MessageEvent) => {
-      if (event.data?.type === 'CUSTOMER_INFO') {
-        setCustomer(event.data.payload); 
-      }
-    };
-
-    window.addEventListener('message', handleMessage);
-    return () => window.removeEventListener('message', handleMessage);
-  }, []);
-
-  return (
-    <div>
-      <h1>QR Code List</h1>
-      {customer && (
+    return (
         <div>
-          <h3>Customer Info:</h3>
-          <pre>{JSON.stringify(customer, null, 2)}</pre>
+            <h1>QR Code List</h1>
+            {customer ? (
+                <div>
+                    <h3>Customer Info Received:</h3>
+                    <pre>{JSON.stringify(customer, null, 2)}</pre>
+                </div>
+            ) : (
+                <div></div>
+            )}
+
+            <ul>
+                <li>1st QR Code</li>
+                <li>2nd QR Code</li>
+                <li>3rd QR Code</li>
+            </ul>
         </div>
-      )}
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </div>
-  );
+    );
 }
