786489d2395a4fd36ca91b1a876670e8bf0241cc
Abh1i <167069151+Abh1i@users.noreply.github.com>
2025-09-30 15:19:23 +0530

    Seventh Change

--- src/app/qr-code-list/page.tsx
+++ src/app/qr-code-list/page.tsx
@@ -2,37 +2,35 @@
 import { useEffect, useState } from 'react';
 
 export default function QrCodeListPage() {
-  const [customer, setCustomer] = useState<any>(null);
+    const [customer, setCustomer] = useState<any>(null);
 
-  useEffect(() => {
-    const handleMessage = (event: MessageEvent) => {
-      if (event.data?.type === 'CUSTOMER_INFO') {
-        setCustomer(event.data.payload);
-      }
-    };
-    window.addEventListener('message', handleMessage);
+    useEffect(() => {
+        const handleMessage = (event: MessageEvent) => {
+            if (event.data?.type === 'CUSTOMER_INFO') {
+                setCustomer(event.data.payload); // <-- shows inside iframe
+            }
+        };
+        window.addEventListener('message', handleMessage);
+        return () => window.removeEventListener('message', handleMessage);
+    }, []);
 
-    return () => window.removeEventListener('message', handleMessage);
-  }, []);
-
-  return (
-    <div>
-      <h1>QR Code List</h1>
-      {customer ? (
+    return (
         <div>
-          <h3>Customer Info Received:</h3>
-          <pre>{JSON.stringify(customer, null, 2)}</pre>
-        </div>
-      ) : (
-        <div></div>
-      )}
+            <h1>QR Code List</h1>
+            {customer ? (
+                <div>
+                    <h3>Customer Info Received:</h3>
+                    <pre>{JSON.stringify(customer, null, 2)}</pre>
+                </div>
+            ) : (
+                <div></div>
+            )}
 
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
-    </div>
-  );
+            <ul>
+                <li>1st QR Code</li>
+                <li>2nd QR Code</li>
+                <li>3rd QR Code</li>
+            </ul>
+        </div>
+    );
 }
